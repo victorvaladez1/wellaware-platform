998aa83e214c8f2b16548e33b8c4978095ad7192
Victor Valadez <vvaladez315@gmail.com>
2025-06-06 11:43:16 -0500

    Update global css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './styles/global.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
